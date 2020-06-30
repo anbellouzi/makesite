d045bf95620c464c3108e0111f72a46e9eabd195
Anas Bellouzi <anbellouzi@gmail.com>
2020-06-29 20:58:23 -0400

    added success message green and bold

--- makesite.go
+++ makesite.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
 	"text/template"
+
+	"github.com/fatih/color"
 )
 
 type Data struct {
@@ -13,18 +16,38 @@ type Data struct {
 	// List []entry
 }
 
+// returns all files within a given directory
+func readFilesFromDir(dir string) []os.FileInfo {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
+// check if fileName is a .txt file return a bool
+func DoesFileExist(fileName string) bool {
+	if strings.Contains(fileName, ".") {
+		return strings.Split(fileName, ".")[1] == "txt"
+	} else {
+		return false
+	}
+}
+
+// creates a fileName.html file using given fileName
 func writeFile(fileName string) *os.File {
 	fileName = strings.Split(fileName, ".")[0] + ".html"
 	file, err := os.Create(fileName)
 	if err != nil {
 		panic(err)
 	}
-
+	// returns the created file
 	return file
 }
 
-func readFromFile(file string) string {
-	fileContents, err := ioutil.ReadFile(file)
+// read content af a given fileName
+func readFromFile(fileName string) string {
+	fileContents, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		// A common use of `panic` is to abort if a function returns an error
 		// value that we don’t know how to (or want to) handle. This example
@@ -34,13 +57,19 @@ func readFromFile(file string) string {
 	return string(fileContents)
 }
 
-func writeTemplate(file string) {
+func writeTemplate(fileName string) {
 	var fileData Data
-	fileData.Content = readFromFile(file)
+	fileData.Content = readFromFile(fileName)
 
 	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
-	newFile := writeFile(file)
+	newFile := writeFile(fileName)
 
+	// print html to stdout
+	// errs := t.Execute(os.Stdout, fileData)		uncomment to print template to stdout
+	// if errs != nil {								uncomment to print template to stdout
+	// 	panic(errs)									uncomment to print template to stdout
+	// }											uncomment to print template to stdout
+	// print html to a file
 	err := t.Execute(newFile, fileData)
 	if err != nil {
 		panic(err)
@@ -48,13 +77,47 @@ func writeTemplate(file string) {
 }
 
 func main() {
+	save()
+}
 
+func print(pageCount int) {
+	// fmt.Print()
+	green := color.New(color.FgGreen).PrintfFunc()
+	green("Success! ")
+	fmt.Print("Generated ")
+	count := color.New(color.Bold, color.FgWhite).PrintFunc()
+	count(pageCount)
+	fmt.Println(" pages")
 }
 
 func save() {
 
 	fileFlag := flag.String("file", "second-post.txt", "file name you want to use for content")
+	dirFlag := flag.String("dir", "", "Directory name that contain all your .txt files")
+
 	flag.Parse()
 
-	writeTemplate(*fileFlag)
+	pageCount := 0
+
+	if *dirFlag != "" {
+		files := readFilesFromDir(*dirFlag)
+
+		for _, file := range files {
+
+			fileName := file.Name()
+			// check if this filename is a txt file
+			if DoesFileExist(fileName) == true {
+				fmt.Println(".txt file found in your dir", fileName)
+				writeTemplate((fileName))
+				pageCount = pageCount + 1
+			}
+		}
+
+	} else {
+		writeTemplate(*fileFlag)
+		pageCount = pageCount + 1
+	}
+
+	print(pageCount)
+
 }
